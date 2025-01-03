766f35078f0d5db6f2fe2c5dbd3ad15ec128ed36
username <wecp-dummy@gmail.com>
2025-01-03 14:03:22 +0000

    wecp initial commit

--- backend/src/main/java/com/wecp/progressive/repository/TeamRepository.java
+++ backend/src/main/java/com/wecp/progressive/repository/TeamRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.wecp.progressive.entity.Team;
 
 public interface TeamRepository extends JpaRepository<Team, Integer> {
+
+    // @Query("")
 }
